235d83cf7df32ebf12513e9914b258cb31761b70
Mo'men Ashraf Mohamed <moamen.ashraf1892001@gmail.com>
2022-07-22 07:17:49 +0200

    commit

---
+++ TaskSchedule/BL/Process.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSchedule.BL
+{
+    public class Process
+    {
+        public String ProcessName { get; set; }
+        public int ArrivalTime { get; set; }
+
+        public int BurstTime { get; set; }
+    }
+}
--- TaskSchedule/Program.cs
+++ TaskSchedule/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TaskSchedule.BL;
 
 namespace TaskSchedule
 {
@@ -6,7 +10,12 @@ namespace TaskSchedule
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            string JsonString = File.ReadAllText("Process.json");
+            List<Process> LstProcesses = JsonConvert.DeserializeObject<List<Process>>(JsonString);
+            string NewString = JsonConvert.SerializeObject(LstProcesses);
+            File.WriteAllText("Process.txt", NewString);
+            Console.ReadKey();
         }
     }
 }
