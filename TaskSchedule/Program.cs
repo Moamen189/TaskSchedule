b96648f3abb33b7807e5888ef13c087415a62b4b
Mo'men Ashraf Mohamed <moamen.ashraf1892001@gmail.com>
2022-07-22 06:41:38 +0200

    Add project files.

---
+++ TaskSchedule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskSchedule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
