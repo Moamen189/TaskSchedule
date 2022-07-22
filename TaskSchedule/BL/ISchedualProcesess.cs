4e4a6f372d5a32f63b51bb85e0cf5a61e945ded3
Mo'men Ashraf Mohamed <moamen.ashraf1892001@gmail.com>
2022-07-22 07:29:52 +0200

    commmit

---
+++ TaskSchedule/BL/ISchedualProcesess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSchedule.BL
+{
+    public interface ISchedualProcesess
+    {
+        public List<Process> MainProcess { get; set; }
+        public List<Process> WaitingProcess { get; set; }
+
+        public Process IncomingProcess(Process CurrentProcess, int TimeLineProcess);
+        public Process IncomingProcess();
+    }
+}
